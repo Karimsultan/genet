82b3d72bd6672910d52fdfca8820aebb2768ee13
Kasia Kozlowska <kasia.kozlowska@arup.com>
2021-12-17 18:03:02 +0000

    add read write network change events

---
+++ genet/use/time_variant_network.py
@@ -0,0 +1,40 @@
+import logging
+import os
+import re
+
+from lxml import etree
+import xml.etree.cElementTree as ET
+
+
+def read_network_change_events_xml(path):
+    change_events = {}
+    ch_event = {'links': set()}
+    for event, elem in ET.iterparse(path):
+        tag = re.sub(r'{http://www\.matsim\.org/files/dtd}', '', elem.tag)  # noqa: W605
+        if tag == 'link':
+            ch_event['links'].add(elem.attrib['refId'])
+        elif tag == 'networkChangeEvent':
+            change_events[elem.attrib['startTime']] = ch_event
+            ch_event = {'links': set()}
+        else:
+            ch_event[tag] = elem.attrib
+    return change_events
+
+def write_network_change_events_xml(change_events, output_dir, fname='networkChangeEvents.xml'):
+    fpath = os.path.join(output_dir, fname)
+    logging.info('Writing {}'.format(fpath))
+
+    with open(fpath, "wb") as f, etree.xmlfile(f, encoding='utf-8') as xf:
+        xf.write_declaration()
+        networkChangeEvents_attribs = {
+            'xmlns': "http://www.matsim.org/files/dtd",
+            'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+            'xsi:schemaLocation': "http://www.matsim.org/files/dtd "
+                                  "http://www.matsim.org/files/dtd/networkChangeEvents.xsd"}
+        with xf.element("networkChangeEvents", networkChangeEvents_attribs):
+            for event_time, change_event in change_events.items():
+                with xf.element("networkChangeEvent", {'startTime': event_time}):
+                    for link in change_event['links']:
+                        xf.write(etree.Element("link", {'refId': link}))
+                    for change_type in set(change_event) - {'links'}:
+                        xf.write(etree.Element(change_type, change_event[change_type]))
---
+++ tests/test_time_variant_network.py
@@ -0,0 +1,72 @@
+import os
+
+import lxml
+import pytest
+from genet.use import time_variant_network
+from lxml.etree import Element
+from tests.fixtures import *
+
+# paths in use assume we're in the repo's root, so make sure we always are
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+os.chdir(root_dir)
+
+
+@pytest.fixture
+def network_change_events_dtd():
+    dtd_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_data/dtd/matsim/networkChangeEvents.xsd"))
+    yield lxml.etree.DTD(dtd_path)
+
+
+@pytest.fixture
+def network_change_events_example_path():
+    sample_xml_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                   "test_data/network_change_events/networkChangeEvents.xml"))
+    yield sample_xml_path
+
+
+@pytest.fixture
+def network_change_events_dict():
+    return {'06:00:00': {'links': {'3', '1', '2'}, 'freespeed': {'type': 'absolute', 'value': '1.0'},
+                         'lanes': {'type': 'absolute', 'value': '2.0'},
+                         'flowCapacity': {'type': 'scaleFactor', 'value': '2'}},
+            '9:00:00': {'links': {'3', '1', '2'}, 'lanes': {'type': 'absolute', 'value': '1.0'},
+                        'freespeed': {'type': 'absolute', 'value': '10.0'},
+                        'flowCapacity': {'type': 'scaleFactor', 'value': '0.5'}}}
+
+
+def test_reading_xml(network_change_events_example_path, network_change_events_dict):
+    ch_events = time_variant_network.read_network_change_events_xml(network_change_events_example_path)
+    assert_semantically_equal(ch_events, network_change_events_dict)
+
+
+def test_writes_well_formed_and_valid_network_change_events_xml_file(tmpdir, network_change_events_dtd, network_change_events_dict):
+    fname = 'net_change_evs.xml'
+    expected_xml = os.path.join(tmpdir, fname)
+    time_variant_network.write_network_change_events_xml(network_change_events_dict, tmpdir, fname=fname)
+
+    xml_obj = lxml.etree.parse(expected_xml)
+    assert network_change_events_dtd.validate(
+        xml_obj), f'Doc generated at {expected_xml} is not valid against ' \
+                  f'DTD due to {network_change_events_dtd.error_log.filter_from_errors()}'
+
+
+def test_saving_network_change_events_to_xml_produces_xml_file(network_change_events_dict, tmpdir):
+    # the content of the file is tested elsewhere
+    fname = 'net_change_evs.xml'
+    expected_xml = os.path.join(tmpdir, fname)
+    assert not os.path.exists(expected_xml)
+
+    time_variant_network.write_network_change_events_xml(network_change_events_dict, tmpdir, fname=fname)
+    assert os.path.exists(expected_xml)
+
+
+
+def test_saving_network_change_events_to_xml_produces_correct_xml_content(network_change_events_dict, tmpdir):
+    fname = 'net_change_evs.xml'
+    expected_xml = os.path.join(tmpdir, fname)
+
+    time_variant_network.write_network_change_events_xml(network_change_events_dict, tmpdir, fname=fname)
+    events_from_file = time_variant_network.read_network_change_events_xml(expected_xml)
+
+    assert_semantically_equal(events_from_file, network_change_events_dict)
+

61b087b7c867038f82a468fb9bb62e1566094257
Kasia Kozlowska <kasia.kozlowska@arup.com>
2022-02-08 16:13:33 +0000

    update simplify script

--- scripts/simplify_network.py
+++ scripts/simplify_network.py
@@ -33,6 +33,18 @@ if __name__ == '__main__':
                             help='The projection network is in, eg. "epsg:27700"',
                             required=True)
 
+    arg_parser.add_argument('-m',
+                            '--simplification_map',
+                            help='Suggested link ID map for simplification. JSON file that is read into a dictionary '
+                                 'with keys that are link IDs in the network being passed and values that are new '
+                                 'link ID strings. For example: `link_simp_map.json` file generated in a previous '
+                                 'simplification attempt. This does not affect WHICH nodes are '
+                                 'simplified, only the resulting link IDs if the IDs and simplification levels match. '
+                                 'For any cases that do not match, new IDs will be generated.',
+                            required=False,
+                            default=None,
+                            )
+
     arg_parser.add_argument('-np',
                             '--processes',
                             help='The number of processes to split computation across',
@@ -50,13 +62,14 @@ if __name__ == '__main__':
     schedule = args['schedule']
     vehicles = args['vehicles']
     projection = args['projection']
+    simplification_map = args['simplification_map']
     processes = args['processes']
     output_dir = args['output_dir']
     ensure_dir(output_dir)
 
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.WARNING)
 
-    logging.info('Reading in network at {}'.format(network))
+    logging.info(f'Reading in network at {network}')
     n = read_matsim(
         path_to_network=network,
         epsg=projection,
@@ -64,16 +77,26 @@ if __name__ == '__main__':
         path_to_vehicles=vehicles
     )
 
+    if simplification_map:
+        logging.info(f'Reading suggested simplification map from {simplification_map}')
+        with open(simplification_map) as json_file:
+            simplification_map = json.load(json_file)
+
     logging.info('Simplifying the Network.')
 
     start = time.time()
-    n.simplify(no_processes=processes)
+    failed_suggested_ids = n.simplify(no_processes=processes, suggested_map=simplification_map)
     end = time.time()
 
     logging.info(
         f'Simplification resulted in {len(n.link_simplification_map)} links being simplified.')
     with open(os.path.join(output_dir, 'link_simp_map.json'), 'w', encoding='utf-8') as f:
         json.dump(n.link_simplification_map, f, ensure_ascii=False, indent=4)
+    with open(os.path.join(output_dir, 'failed_suggested_simp_ids.json'), 'w', encoding='utf-8') as f:
+        json.dump(
+            {'failed_suggested_ids': list(failed_suggested_ids),
+             'suggested_map': simplification_map},
+            f, ensure_ascii=False, indent=4)
 
     n.write_to_matsim(output_dir)
 
